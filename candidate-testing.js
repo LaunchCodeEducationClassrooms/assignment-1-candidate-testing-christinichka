9513ae8c1a25dfe29c0355c773078a63268deaae
Christina Varghese <christina.varghese@gmail.com>
2021-02-16 19:08:45 +0000

    Edited the code to repair the errors I received by the auto grader.

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,17 +10,12 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  'Who was the first American woman in space?: ', 
-  'True or False: 5000 meters = 5 kilometers: ', 
-  '(5+3)/2 * 10 = ', 
-  "Given the array [8, 'Orbit' , 'Trajectory', 45], what entry is at index 2?: ", 
-  'What is the minimum crew size for the International Space Station (ISS)?: '];
-let correctAnswers = [
-  "Sally Ride", 
-  "True",
-  "40", 
-  "Trajectory", 
-  "3"];
+  'Who was the first American woman in space? ', 
+  'True or False: 5000 meters == 5 kilometers ', 
+  '(5+3)/2 * 10 = ? ', 
+  'Given the array [8, "Orbit" , "Trajectory", 45], what entry is at index 2? ', 
+  'What is the minimum crew size for ISS? '];
+let correctAnswers = ['Sally Ride','True','40','Trajectory','3'];
 let candidateAnswers = [];
 
 function askForName() {
