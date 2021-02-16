ae580008be6af60b59feb5032a1c0cdcbb2bad52
Christina Varghese <christina.varghese@gmail.com>
2021-02-16 19:25:35 +0000

    Took an extra space out in one question. Also changed one number per the auto grader.

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ['Who was the first American woman in space? ', 'True or false: 5000 meters == 5 kilometers? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit' , 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+let questions = ['Who was the first American woman in space? ', 'True or false: 5000 meters == 5 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswers = [];
 
